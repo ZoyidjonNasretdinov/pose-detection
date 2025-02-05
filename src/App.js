df9bcd289634d2f46d70a52311f42ace0d079fac
ZoyidjonNasretdinov <zoyidjonnasretdinovcoder@gmail.com>
2025-02-05 19:47:59 +0500

    Import package

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import {useRef, useEffect} from 'react'
+import * as tf from '@tensorflow/tfjs'
+import * as poseNet from '@tensorflow-models/posenet'
+import Webcam from 'react-webcam'
+import './App.css'
+
+const App = () => {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
